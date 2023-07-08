8513051f7bbff91b4b73d05cfff3c40e71616324
Lenny <witnneygabriel13@gmail.com>
2023-07-08 17:58:06 -0300

    feat: add recommended trips component

---
+++ src/app/components/RecommendedTrips.tsx
@@ -0,0 +1,16 @@
+export function RecommendedTrips() {
+  return (
+    <div className="container mx-auto p-5">
+      <div className="w-full h-[1px] bg-grayLighter"></div>
+      <h2 className="font-medium text-grayPrimary whitespace-nowrap px-5">
+        Destinos Recomendados
+      </h2>
+      <div className="w-full h-[1px] bg-grayLighter"></div>
+
+    <div className="flex flex-col items-center mt-5 gap-5">
+      
+    </div>
+
+    </div>
+  )
+}
